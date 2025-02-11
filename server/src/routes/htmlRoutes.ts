b4af9da7a0963e53763eeae5198fe8553583d9d1
Timothy Ehli <tehli1882@gmail.com>
2025-02-11 12:02:41 -0500

    Fix import.meta.url issue

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,5 +1,5 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { Router, type Request, type Response } from 'express';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
