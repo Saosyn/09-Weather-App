6ee08c2db1c34632eadc2a59034060e197f9d792
Timothy Ehli <tehli1882@gmail.com>
2025-02-24 12:49:17 -0500

    updated htmlRoutes again

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -9,11 +9,11 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 // ✅ Serve static files (CSS, JS, images) from client/
-router.use(express.static(path.join(__dirname, '../../client')));
+router.use(express.static(path.join(__dirname, 'client')));
 
 // ✅ Serve index.html for all routes
 router.get('*', (_req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../../client/index.html'));
+  res.sendFile(path.join(process.cwd(), 'client/index.html'));
 });
 
 export default router;
