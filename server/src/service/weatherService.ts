faea6748e4b915c2ae15deeb90f983474ec9c2aa
Timothy Ehli <tehli1882@gmail.com>
2025-02-24 14:21:13 -0500

    trying to get wind to pupulate instead of undefined. changed day.wind.speed in weatherService.ts

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -132,7 +132,7 @@ class WeatherService {
     return new Weather(
       this.city,
       response.main.temp,
-      response.wind.speed,
+      response.main.wind_speed,
       response.main.humidity,
       parsedDate,
       response.weather[0].icon,
@@ -162,7 +162,7 @@ class WeatherService {
         new Weather(
           this.city,
           day.main.temp,
-          day.wind.speed,
+          day.main.wind_speed,
           day.main.humidity,
           formattedDate,
           day.weather[0].icon,
